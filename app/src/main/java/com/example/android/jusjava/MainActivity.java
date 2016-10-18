18ea69041ec3281fa77d7bca6ebee74d8b1ac318
gautam <gautamkrishnan7209@gmail.com>
2016-10-18 13:45:58 +0530

    Adding topping

--- app/src/main/java/com/example/android/jusjava/MainActivity.java
+++ app/src/main/java/com/example/android/jusjava/MainActivity.java
@@ -1,10 +1,11 @@
 /**
  * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
  * This is the package name our example uses:
- *
+ * <p>
  * package com.example.android.justjava;
  */
 package com.example.android.jusjava;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,7 +18,8 @@ import com.example.android.jusjava.R;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity=0;
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,22 +30,32 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String coffees="Total count of coffees = "+quantity+"\n Price=$"+(quantity*5);
-        coffees=coffees+"\nThank you!\nPlease visit again";
+        String coffees = "Name:Gautam Krishnan\nTotal count of coffees = " + quantity + "\n Price=$" + (calculatePrice());
+        coffees = coffees + "\nThank you!\nPlease visit again";
         displayMessagePrice(coffees);
     }
+
+    /**
+     * This method calculates the price
+     * @return total price
+     */
+    public int calculatePrice() {
+        return quantity * 5;
+    }
+
     /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
-        quantity=quantity+1;
+        quantity = quantity + 1;
         display(quantity);
     }
+
     /**
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
-        quantity=quantity-1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
@@ -52,19 +64,12 @@ public class MainActivity extends AppCompatActivity {
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" +number);
+        quantityTextView.setText("" + number);
 
     }
-    /**
-     * This method displays the given price value on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText("$" + number);
-    }
-    private void displayMessagePrice(String msg){
-        TextView priceTextView=(TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(msg);
 
+    private void displayMessagePrice(String msg) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(msg);
     }
 }
