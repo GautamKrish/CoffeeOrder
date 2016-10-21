1ab0383c13ff6dd8cff1abe12e70987be8adac10
gautam <gautamkrishnan7209@gmail.com>
2016-10-21 19:14:03 +0530

    Connecting to g-mail

--- app/src/main/java/com/example/android/jusjava/MainActivity.java
+++ app/src/main/java/com/example/android/jusjava/MainActivity.java
@@ -6,19 +6,27 @@
  */
 package com.example.android.jusjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.jusjava.R;
+
+import static android.R.attr.checked;
+import static android.view.View.Y;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +38,49 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String coffees = "Name:Gautam Krishnan\nTotal count of coffees = " + quantity + "\n Price=$" + (calculatePrice());
-        coffees = coffees + "\nThank you!\nPlease visit again";
-        displayMessagePrice(coffees);
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+        CheckBox hasWhippedCream = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean checkWhippedCream = hasWhippedCream.isChecked();
+        CheckBox hasChocolateTopping = (CheckBox) findViewById(R.id.chocolate_topping);
+        boolean checkChocolateTopping = hasChocolateTopping.isChecked();
+        String coffees = "Name:" + name;
+        coffees += "\nAdd whipped cream? " + checkWhippedCream;
+        coffees += "\nAdd Chocolate Topping? " + checkChocolateTopping;
+        coffees += "\nTotal count of coffees = " + quantity + "\n Price=$" + (calculatePrice(checkWhippedCream, checkChocolateTopping));
+        coffees += "\nThank you!\nPlease visit again";
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Jus Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, coffees);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
+
 
     /**
      * This method calculates the price
+     *
      * @return total price
      */
-    public int calculatePrice() {
-        return quantity * 5;
+    public int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int baseprice = 5;
+        if (addWhippedCream)
+            baseprice = baseprice + 1;
+        if (addChocolate)
+            baseprice = baseprice + 3;
+        return quantity * baseprice;
     }
 
     /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         display(quantity);
     }
@@ -55,6 +89,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 1) {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity - 1;
         display(quantity);
     }
@@ -68,8 +106,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void displayMessagePrice(String msg) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(msg);
-    }
 }
